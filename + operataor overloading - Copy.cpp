13b18e1d36289c18c4e3b452e197f5471cac06f1
Aman India <73029535+Indianspices@users.noreply.github.com>
2020-10-18 01:16:07 +0530

    Operator overloading in C++

---
+++ + operataor overloading - Copy.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+ class goal
+ {
+ 	private :
+ 		int a,b;
+ 		public :
+ 			void setdata(int x,int y)
+ 			{
+ 				a=x;
+ 				b=y;
+ 				
+			 }
+			 void showdata()
+			 {
+			 	cout<<"The value of 'a' is"<<" "<<a<<" and the value of 'b' is "<<" "<<b<<".";
+			 }
+			 friend goal operator +(goal);
+ };
+ goal operator +(goal g)
+ {
+ 	goal temp;
+ 	temp.a=g.a;
+ 	temp.b=g.b;
+ 	return (temp);
+ }
+ int main()
+ {
+ 	goal g1,g2;
+ 	g1.setdata(45,56);
+ 	g2=+g1;//g2=operator +(g1)
+ 	g2.showdata();
+ 	
+ 	
+ }
+ 
+
+
